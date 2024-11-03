369fac793b0c6b266662b65b6dcb1fde8ef8970d
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-11-04 04:03:47 +0700

    upd

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # User-provided configuration must always be respected.
+#
 # Therefore, this script must only derives Airflow AIRFLOW__ variables from other variables
 # when the user did not provide their own configuration.
 
@@ -10,9 +11,6 @@ TRY_LOOP="20"
 : "${AIRFLOW_HOME:="/usr/local/airflow"}"
 : "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
 : "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
-
-# Tambahkan variabel MONGO_URI jika tidak didefinisikan oleh pengguna
-: "${MONGO_URI:="mongodb://admin:admin@mongo:27017/airflow"}"
 
 # Load DAGs examples (default: Yes)
 if [[ -z "$AIRFLOW__CORE__LOAD_EXAMPLES" && "${LOAD_EX:=n}" == n ]]; then
@@ -24,7 +22,6 @@ export \
   AIRFLOW__CORE__EXECUTOR \
   AIRFLOW__CORE__FERNET_KEY \
   AIRFLOW__CORE__LOAD_EXAMPLES \
-  MONGO_URI  # Export MONGO_URI agar bisa digunakan di aplikasi
 
 # Install custom python package if requirements.txt is present
 if [ -e "/requirements.txt" ]; then
@@ -45,15 +42,11 @@ wait_for_port() {
   done
 }
 
-# Check if the user has provided explicit Airflow configuration concerning the MongoDB
-if [ -n "$MONGO_URI" ]; then
-  # Gunakan `MONGO_URI` di sini jika aplikasi atau script Anda memerlukan koneksi ke MongoDB.
-  echo "Using MongoDB URI: $MONGO_URI"
-fi
-
-# PostgreSQL untuk backend Airflow
+# Other executors than SequentialExecutor drive the need for an SQL database, here PostgreSQL is used
 if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
+  # Check if the user has provided explicit Airflow configuration concerning the database
   if [ -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]; then
+    # Default values corresponding to the default compose files
     : "${POSTGRES_HOST:="postgres"}"
     : "${POSTGRES_PORT:="5432"}"
     : "${POSTGRES_USER:="airflow"}"
@@ -64,23 +57,38 @@ if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
     AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
     export AIRFLOW__CORE__SQL_ALCHEMY_CONN
 
+    # Check if the user has provided explicit Airflow configuration for the broker's connection to the database
     if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
       AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
       export AIRFLOW__CELERY__RESULT_BACKEND
     fi
+  else
+    if [[ "$AIRFLOW__CORE__EXECUTOR" == "CeleryExecutor" && -z "$AIRFLOW__CELERY__RESULT_BACKEND" ]]; then
+      >&2 printf '%s\n' "FATAL: if you set AIRFLOW__CORE__SQL_ALCHEMY_CONN manually with CeleryExecutor you must also set AIRFLOW__CELERY__RESULT_BACKEND"
+      exit 1
+    fi
+
+    # Derive useful variables from the AIRFLOW__ variables provided explicitly by the user
+    POSTGRES_ENDPOINT=$(echo -n "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" | cut -d '/' -f3 | sed -e 's,.*@,,')
+    POSTGRES_HOST=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f1)
+    POSTGRES_PORT=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f2)
   fi
+
   wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
 fi
 
-# CeleryExecutor membutuhkan Redis sebagai broker
+# CeleryExecutor drives the need for a Celery broker, here Redis is used
 if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
+  # Check if the user has provided explicit Airflow configuration concerning the broker
   if [ -z "$AIRFLOW__CELERY__BROKER_URL" ]; then
+    # Default values corresponding to the default compose files
     : "${REDIS_PROTO:="redis://"}"
     : "${REDIS_HOST:="redis"}"
     : "${REDIS_PORT:="6379"}"
     : "${REDIS_PASSWORD:=""}"
     : "${REDIS_DBNUM:="1"}"
 
+    # When Redis is secured by basic auth, it does not handle the username part of basic auth, only a token
     if [ -n "$REDIS_PASSWORD" ]; then
       REDIS_PREFIX=":${REDIS_PASSWORD}@"
     else
@@ -89,20 +97,27 @@ if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
 
     AIRFLOW__CELERY__BROKER_URL="${REDIS_PROTO}${REDIS_PREFIX}${REDIS_HOST}:${REDIS_PORT}/${REDIS_DBNUM}"
     export AIRFLOW__CELERY__BROKER_URL
+  else
+    # Derive useful variables from the AIRFLOW__ variables provided explicitly by the user
+    REDIS_ENDPOINT=$(echo -n "$AIRFLOW__CELERY__BROKER_URL" | cut -d '/' -f3 | sed -e 's,.*@,,')
+    REDIS_HOST=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f1)
+    REDIS_PORT=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f2)
   fi
+
   wait_for_port "Redis" "$REDIS_HOST" "$REDIS_PORT"
 fi
 
-# Case logic to start services
 case "$1" in
   webserver)
     airflow initdb
     if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ] || [ "$AIRFLOW__CORE__EXECUTOR" = "SequentialExecutor" ]; then
+      # With the "Local" and "Sequential" executors it should all run in one container.
       airflow scheduler &
     fi
     exec airflow webserver
     ;;
   worker|scheduler)
+    # Give the webserver time to run initdb.
     sleep 10
     exec airflow "$@"
     ;;
@@ -114,6 +129,7 @@ case "$1" in
     exec airflow "$@"
     ;;
   *)
+    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
     exec "$@"
     ;;
 esac
